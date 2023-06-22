43c2392429b42a6c5afa8746e871d35df307286d
lucasmuar <lucasma3005@gmail.com>
2023-06-22 15:16:04 +0200

    change config-data

--- coche-service/src/main/java/com/cocheservice/CocheServiceApplication.java
+++ coche-service/src/main/java/com/cocheservice/CocheServiceApplication.java
@@ -3,6 +3,13 @@ package com.cocheservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,4 +19,18 @@ public class CocheServiceApplication {
 		SpringApplication.run(CocheServiceApplication.class, args);
 	}
 
+}
+
+
+@RestController
+class ServiceInstanceRestController {
+
+	@Autowired
+	private DiscoveryClient discoveryClient;
+
+	@RequestMapping("/service-instances/{applicationName}")
+	public List<ServiceInstance> serviceInstancesByApplicationName(
+			@PathVariable String applicationName) {
+		return this.discoveryClient.getInstances(applicationName);
+	}
 }
